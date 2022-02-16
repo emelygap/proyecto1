c61fcb26dd7a6233b8f40450c327b934599ea5d4
emelygap <mily18gaby@outlook.com>
2022-02-16 02:36:33 +0100

    Validando Cambio2 del Proyecto1

---
+++ prueba1/src/cambio1/Cambio2.java
@@ -0,0 +1,11 @@
+package cambio1;
+
+public class Cambio2 {
+
+	public static void main(String[] args) {
+		System.out.println("Clase cambio2 en proyecto1");
+		System.out.println("Clase cambio2 en proyecto1");
+
+	}
+
+}
